c9225d04755d92ab3fa49cc03c7bd21484836b45
SL <Lum_Weiling@hotmail.com>
2014-08-11 18:18:38 +0800

    Fix ios7.1 clear notification
    Fix ios7.1 clear notifications after it has been clicked.

--- src/ios/AppDelegate+notification.m
+++ src/ios/AppDelegate+notification.m
@@ -1,119 +1,32 @@
-//
-//  AppDelegate+notification.m
-//  pushtest
-//
-//  Created by Robert Easterday on 10/26/12.
-//
-//
+		function addNotification(team, result, isParseObject, promises, customMessage){
+			//Create a notification record sent to user with pending invites
+			var notification = {
+				data: {
+					message: customMessage, 
+					senderType: NotificationModule.SenderType.Team, 
+					senderId: team.id, 
+					senderName: team.attributes.name, 
+					senderImageUrl: team.attributes.picture
+				},
+				recipient: isParseObject ? result.get("user") : result.user,
+				type: NotificationModule.Type.InvitationResponse,
+				status: NotificationModule.Status.Unread,
+				challenge: team.get("challenge"),
+				requireAction: false,
+				actionPerformed: null
+			}
+			promises.push(NotificationModule.save(notification));
 
-#import "AppDelegate+notification.h"
-#import "CDVParsePlugin.h"
-#import <objc/runtime.h>
-
-static char launchNotificationKey;
-
-@implementation AppDelegate (notification)
-
-- (id) getCommandInstance:(NSString*)className
-{
-	return [self.viewController getCommandInstance:className];
-}
-
-// its dangerous to override a method from within a category.
-// Instead we will use method swizzling. we set this up in the load call.
-+ (void)load
-{
-    Method original, swizzled;
-    
-    original = class_getInstanceMethod(self, @selector(init));
-    swizzled = class_getInstanceMethod(self, @selector(swizzled_init));
-    method_exchangeImplementations(original, swizzled);
-}
-
-- (AppDelegate *)swizzled_init
-{
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(createNotificationChecker:)
-                                                 name:@"UIApplicationDidFinishLaunchingNotification" object:nil];
-	
-	// This actually calls the original init method over in AppDelegate. Equivilent to calling super
-	// on an overrided method, this is not recursive, although it appears that way. neat huh?
-	return [self swizzled_init];
-}
-
-// This code will be called immediately after application:didFinishLaunchingWithOptions:. We need
-// to process notifications in cold-start situations
-- (void)createNotificationChecker:(NSNotification *)notification
-{
-	if (notification)
-	{
-		NSDictionary *launchOptions = [notification userInfo];
-		if (launchOptions)
-			self.launchNotification = [launchOptions objectForKey: @"UIApplicationLaunchOptionsRemoteNotificationKey"];
-	}
-}
-
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    CDVParsePlugin *pushHandler = [self getCommandInstance:@"ParsePlugin"];
-    [pushHandler didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
-}
-
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-    CDVParsePlugin *pushHandler = [self getCommandInstance:@"ParsePlugin"];
-    [pushHandler didFailToRegisterForRemoteNotificationsWithError:error];
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-    NSLog(@"didReceiveRemoteNotification");
-    
-    // Get application state for iOS4.x+ devices, otherwise assume active
-    UIApplicationState appState = UIApplicationStateActive;
-    if ([application respondsToSelector:@selector(applicationState)]) {
-        appState = application.applicationState;
-    }
-    
-    if (appState == UIApplicationStateActive) {
-        NSLog(@"App is active while receiving push notification");
-        CDVParsePlugin *pushHandler = [self getCommandInstance:@"ParsePlugin"];
-        pushHandler.pendingNotifications = userInfo;
-        //pushHandler.isInline = YES;
-        [pushHandler flushNotificationToClient];
-    } else {
-        //save it for later
-        self.launchNotification = userInfo;
-    }
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    
-    NSLog(@"active");
-    
-    //zero badge
-    application.applicationIconBadgeNumber = 0;
-    
-    if (![self.viewController.webView isLoading] && self.launchNotification) {
-        CDVParsePlugin *pushHandler = [self getCommandInstance:@"ParsePlugin"];
-		
-        pushHandler.pendingNotifications = self.launchNotification;
-        self.launchNotification = nil;
-        [pushHandler performSelectorOnMainThread:@selector(flushNotificationToClient) withObject:pushHandler waitUntilDone:NO];
-    }
-}
-
-// The accessors use an Associative Reference since you can't define a iVar in a category
-// http://developer.apple.com/library/ios/#documentation/cocoa/conceptual/objectivec/Chapters/ocAssociativeReferences.html
-- (NSMutableArray *)launchNotification
-{
-    return objc_getAssociatedObject(self, &launchNotificationKey);
-}
-
-- (void)setLaunchNotification:(NSDictionary *)aDictionary
-{
-    objc_setAssociatedObject(self, &launchNotificationKey, aDictionary, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-- (void)dealloc
-{
-    self.launchNotification	= nil; // clear the association and release the object
-}
-
-@end
+			//Send a push notification
+			var pushNotification = {
+				text: notification.data.message,
+				target: {
+					type: PushNotificationModule.Target.User,
+					user: notification.recipient
+				},
+				data: {
+					type: notification.type
+				}
+			}
+			promises.push(PushNotificationModule.send(pushNotification));
+		}
