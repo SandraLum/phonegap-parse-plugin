f81995fb6e3e211455180dbd113da8d39fa44e9c
SandraLum <sandra.lum@SGPC054.leotech.local>
2014-08-11 15:54:36 +0800

    Updates ios chanes

--- src/ios/CDVParsePlugin.h
+++ src/ios/CDVParsePlugin.h
@@ -2,15 +2,23 @@
 #import "AppDelegate.h"
 
 @interface CDVParsePlugin: CDVPlugin
+{
+    NSDictionary *pendingNotifications;
+    NSString *listenerCallbackId;
+}
 
+@property (nonatomic, copy) NSString *listenerCallbackId;
+@property (nonatomic, strong) NSDictionary *pendingNotifications;
+
+- (void)didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
+- (void)didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
 - (void)initialize: (CDVInvokedUrlCommand*)command;
 - (void)getInstallationId: (CDVInvokedUrlCommand*)command;
 - (void)getInstallationObjectId: (CDVInvokedUrlCommand*)command;
 - (void)getSubscriptions: (CDVInvokedUrlCommand *)command;
 - (void)subscribe: (CDVInvokedUrlCommand *)command;
 - (void)unsubscribe: (CDVInvokedUrlCommand *)command;
-
-@end
-
-@interface AppDelegate (CDVParsePlugin)
+- (void)registerListener: (CDVInvokedUrlCommand *)command;
+- (void)flushNotifications: (CDVInvokedUrlCommand *)command;
+- (void)flushNotificationToClient;
 @end
